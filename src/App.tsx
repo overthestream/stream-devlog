988f8b44b43335948e2ecfe04c45e2b780a89608
overthestream <njhnjh02@naver.com>
2022-03-13 16:47:46 +0900

    chore: fix directory tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import HomePage from 'pages/HomePage';
 import React from 'react';
-import GlobalStyle from 'components/base/GlobalStyle';
+import GlobalStyle from 'assets/GlobalStyle';
 
 const App: React.FC = () => {
   return (
